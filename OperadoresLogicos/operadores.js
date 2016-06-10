7e586fab4573049cd85296f4bf4e7057d76aab81
felixacdc <pfmata01@gmail.com>
2016-06-09 23:30:19 -0600

    [add] 7. Curso de JavaScript ES6, Operadores Lógicos

---
+++ OperadoresLogicos/operadores.js
@@ -0,0 +1,25 @@
+/*
+OPERADOR        DESCRIPCION
+    ==              Igual a
+    !=              Direrente que
+    <               Menor que
+    <=              Menor o igual que
+    >               Mayor que
+    >=              Mayor o igual que
+    &&              Operador and (y)
+    ||              Operador or (o)
+    !               Operador not (no)
+*/
+
+console.log(5 == 6);
+console.log(5 != 6);
+console.log(5 < 5);
+console.log(5 <= 5);
+console.log(5 > 6);
+console.log(5 >= 6);
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || true);
+console.log(true || false);
+console.log(!false);
