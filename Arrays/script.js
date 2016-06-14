10e658f04042d2cb3066981253349bd0fc032be1
felixacdc <pfmata01@gmail.com>
2016-06-14 09:22:24 -0600

    [add] 9. Curso de JavaScript ES6, Arrays

---
+++ Arrays/script.js
@@ -0,0 +1,11 @@
+// Arrays
+
+var languages = new Array('Ruby', 'Javascript', 'PHP');
+
+languages.push('Swift'); //Agrega un elemento al array
+//languages.pop(); //Elimina el ultimo elemento del array
+
+console.log(languages.sort()); // Ordena en forma alfabetica los elementos del array
+console.log(languages.length); // Obtiene el numero de elementos del array
+console.log(languages[0]);
+console.log(languages[3]);
