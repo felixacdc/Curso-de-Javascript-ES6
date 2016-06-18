5259a16697f3a763087e570c496b6a98bc0ba651
felixacdc <pfmata01@gmail.com>
2016-06-18 13:58:56 -0600

    [add] 19. Curso de Javascript ES6, Parametros por default

---
+++ ParametrosDefault/functions.js
@@ -0,0 +1,9 @@
+/*
+Parametros por default
+*/
+
+function person (name = "Sin Nombre", age = "Sin Edad") {
+    console.log(`Hola mi nombre es ${name} y tengo ${age} años`);
+}
+
+person("Felix", 22);
