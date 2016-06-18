869b93fa1dad37e42a4a1904b5cbbfb70cf153ec
felixacdc <pfmata01@gmail.com>
2016-06-18 15:51:52 -0600

    [add] 23. Curso de Javascript ES6, Clases

---
+++ Clases/class.js
@@ -0,0 +1,14 @@
+class Dog {
+
+    constructor (name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+}
+
+var dog = new Dog("Popi", "Golden");
+
+console.log(dog);
+console.log(dog.name);
+console.log(dog.color);
