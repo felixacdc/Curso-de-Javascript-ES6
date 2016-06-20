da8862745969c5ce8e45976de559f19d0e1ebed8
felixacdc <pfmata01@gmail.com>
2016-06-19 22:14:21 -0600

    [add] 28. Curso de Javascript ES6, Number Object

---
+++ Number/script.js
@@ -0,0 +1,12 @@
+// Validar que un numero sea numero
+console.log(Number.isNaN(12));
+console.log(Number.isNaN(0/0));
+
+// Valida que el numero sea finito
+console.log(Number.isFinite(5));
+console.log(Number.isFinite(Infinity));
+
+// Valida si un numero es entero
+console.log(Number.isInteger(10));
+console.log(Number.isInteger("hoal"));
+console.log(Number.isInteger(10.5));
