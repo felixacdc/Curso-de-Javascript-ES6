57210b29d6c18f1888fe042db90b48bfc51262d9
felixacdc <pfmata01@gmail.com>
2016-06-19 21:25:53 -0600

    [add] 27. Curso de Javascript ES6, String Object

---
+++ Strings/script.js
@@ -0,0 +1,17 @@
+var cadena = "abc";
+var curso = "Curso de Javascript 2016";
+
+// Repetir cadena
+console.log(cadena.repeat(5));
+
+// Buscar si una cadena contiene un texto
+console.log(curso.includes("Javascript"));
+
+// Buscar si una cadena contiene un texto en cierto indice
+console.log(curso.includes("de", 6));
+
+// Verifica si una cadena comienza con un texto
+console.log(curso.startsWith("Curso"));
+
+// Verifica si una cadena termina con un texto
+console.log(curso.endsWith("2016"));
